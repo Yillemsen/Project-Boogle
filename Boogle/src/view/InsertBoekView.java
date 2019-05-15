0615003a52e51256bd5a72af5426bb7fad6d52d6
Sunnywan1 <47242099+Sunnywan1@users.noreply.github.com>
2019-05-15 15:15:41 +0200

    Schermen gedaan, moet nog boekrek en filmrek toevoegen

--- Boogle/src/view/InsertBoekView.java
+++ Boogle/src/view/InsertBoekView.java
@@ -1,12 +1,61 @@
 package view;
 
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class InsertBoekView extends GridPane {
-
+    private Label lblIsbn, lblBooknumber, lblTitle, lblLanguage, lblDate, lblIntTitle, lblGenre;
+    private TextField txtIsbn, txtBooknumber, txtTitle, txtLanguage, txtDate, txtIntTitle, txtGenre;
+    private Text bookInput;
+    private Button btnSave;
+    
 	public InsertBoekView(Pane mainPane) {
-		
+            lblIsbn = new Label(" ISBN : ");
+            lblBooknumber = new Label(" Boeknummer : ");
+            lblTitle = new Label(" Titel : ");
+            lblLanguage = new Label(" Taal : ");
+            lblDate = new Label(" Datum uitgave : ");
+            lblIntTitle = new Label(" Internationale titel : ");
+            lblGenre = new Label(" Genre : ");
+            
+            txtIsbn = new TextField();
+            txtBooknumber = new TextField();
+            txtTitle = new TextField();
+            txtLanguage = new TextField();
+            txtDate  = new TextField();
+            txtIntTitle = new TextField();
+            txtGenre = new TextField();
+            
+            bookInput = new Text(" Boek invoeren ");
+            
+            btnSave = new Button(" Opslaan ");
+            
+            setPadding(new Insets(10,10,10,10));
+            setHgap(10);
+            setVgap(10);
+        
+            add(bookInput, 0, 0); 
+            add(lblIsbn, 0, 2);
+            add(txtIsbn, 1, 2);
+            add(lblBooknumber, 0, 3);
+            add(txtBooknumber, 1, 3);
+            add(lblTitle, 0, 4);
+            add(txtTitle, 1, 4);
+            add(lblLanguage, 0, 5);
+            add(txtLanguage, 1, 5);
+            add(lblDate, 0, 6);
+            add(txtDate, 1, 6);
+            add(lblIntTitle, 0, 7);
+            add(txtIntTitle, 1, 7);
+            add(lblGenre, 0, 8);
+            add(txtGenre, 1, 8);
+            add(btnSave, 1, 9);
+
+            mainPane.getChildren().add(this);
 	}
 }
--- Boogle/src/view/InsertFilmView.java
+++ Boogle/src/view/InsertFilmView.java
@@ -1,11 +1,62 @@
 package view;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class InsertFilmView extends GridPane{
+    private Label lblTitle, lblMovienumber, lblReg, lblDate, lblDesc, lblGenre;
+    private TextField txtTitle, txtMovienumber, txtReg, txtDate, txtGenre;
+    private Text movieInput;
+    private TextArea txtDesc;
+    private Button btnSave;
 	
 	public InsertFilmView(Pane mainPane) {
+            lblTitle = new Label(" Titel : ");
+            lblMovienumber = new Label(" Filmnummer : ");
+            lblReg = new Label(" Regiseur : ");
+            lblDate = new Label(" Premiere datum : ");
+            lblDesc = new Label(" Beschrijving : ");
+            lblGenre = new Label(" Genre : ");
+            
+            txtTitle = new TextField();
+            txtMovienumber = new TextField();
+            txtReg = new TextField();
+            txtDate = new TextField();
+            txtDesc  = new TextArea();
+            txtDesc.setPrefHeight(150);
+            txtDesc.setPrefWidth(20);
+            txtGenre = new TextField();
+            
+            movieInput = new Text(" Film invoeren ");
+            
+            btnSave = new Button(" Opslaan ");
+            
+            setPadding(new Insets(10,10,10,10));
+            setHgap(10);
+            setVgap(10);
+        
+            add(movieInput, 0, 0); 
+            add(lblTitle, 0, 2);
+            add(txtTitle, 1, 2);
+            add(lblMovienumber, 0, 3);
+            add(txtMovienumber, 1, 3);
+            add(lblReg, 0, 4);
+            add(txtReg, 1, 4);
+            add(lblDate, 0, 5);
+            add(txtDate, 1, 5);
+            add(lblDesc, 0, 6);
+            add(txtDesc, 1, 6);
+            add(lblGenre, 0, 7);
+            add(txtGenre, 1, 7);
+            add(btnSave, 1, 8);
+
+            mainPane.getChildren().add(this);
 		
 	}
 
