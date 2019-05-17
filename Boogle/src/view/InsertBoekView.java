ef6031116ee57b479b3646c6cfd092ff31604cc7
Sunnywan1 <47242099+Sunnywan1@users.noreply.github.com>
2019-05-17 12:11:08 +0200

    Extra toegevoegd bij

--- Boogle/src/view/InsertBoekView.java
+++ Boogle/src/view/InsertBoekView.java
@@ -2,6 +2,7 @@ package view;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -9,12 +10,16 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class InsertBoekView extends GridPane {
-    private Label lblIsbn, lblBooknumber, lblTitle, lblLanguage, lblDate, lblIntTitle, lblGenre;
-    private TextField txtIsbn, txtBooknumber, txtTitle, txtLanguage, txtDate, txtIntTitle, txtGenre;
+    private Label lblIsbn, lblBooknumber, lblTitle, lblLanguage, lblDate, lblIntTitle, lblGenre, lblAuteur, lblLibary, lblNumber;
+    private TextField txtIsbn, txtBooknumber, txtTitle, txtDate, txtIntTitle;
+    private ComboBox boxLanguage, boxGenre, boxAuteur, boxLibary, boxNumber;
     private Text bookInput;
     private Button btnSave;
     
 	public InsertBoekView(Pane mainPane) {
+            //Labels to show
+            lblLibary = new Label (" Bibliotheek : ");
+            lblNumber = new Label (" Kastnummer : ");
             lblIsbn = new Label(" ISBN : ");
             lblBooknumber = new Label(" Boeknummer : ");
             lblTitle = new Label(" Titel : ");
@@ -22,14 +27,20 @@ public class InsertBoekView extends GridPane {
             lblDate = new Label(" Datum uitgave : ");
             lblIntTitle = new Label(" Internationale titel : ");
             lblGenre = new Label(" Genre : ");
+            lblAuteur = new Label(" Auteur : ");
             
+            
+            //TextFields and ComboBoxs to show
+            boxLibary = new ComboBox();
+            boxNumber = new ComboBox();
             txtIsbn = new TextField();
             txtBooknumber = new TextField();
             txtTitle = new TextField();
-            txtLanguage = new TextField();
+            boxLanguage = new ComboBox();
             txtDate  = new TextField();
             txtIntTitle = new TextField();
-            txtGenre = new TextField();
+            boxGenre = new ComboBox();
+            boxAuteur = new ComboBox();
             
             bookInput = new Text(" Boek invoeren ");
             
@@ -40,21 +51,27 @@ public class InsertBoekView extends GridPane {
             setVgap(10);
         
             add(bookInput, 0, 0); 
-            add(lblIsbn, 0, 2);
-            add(txtIsbn, 1, 2);
-            add(lblBooknumber, 0, 3);
-            add(txtBooknumber, 1, 3);
-            add(lblTitle, 0, 4);
-            add(txtTitle, 1, 4);
-            add(lblLanguage, 0, 5);
-            add(txtLanguage, 1, 5);
-            add(lblDate, 0, 6);
-            add(txtDate, 1, 6);
-            add(lblIntTitle, 0, 7);
-            add(txtIntTitle, 1, 7);
-            add(lblGenre, 0, 8);
-            add(txtGenre, 1, 8);
-            add(btnSave, 1, 9);
+            add(lblLibary, 0, 2);
+            add(boxLibary, 1, 2);
+            add(lblNumber, 0, 3);
+            add(boxNumber, 1, 3);
+            add(lblIsbn, 0, 4);
+            add(txtIsbn, 1, 4);
+            add(lblBooknumber, 0, 5);
+            add(txtBooknumber, 1, 5);
+            add(lblTitle, 0, 6);
+            add(txtTitle, 1, 6);
+            add(lblLanguage, 0, 7);
+            add(boxLanguage, 1, 7);
+            add(lblDate, 0, 8);
+            add(txtDate, 1, 8);
+            add(lblIntTitle, 0, 9);
+            add(txtIntTitle, 1, 9);
+            add(lblGenre, 0, 10);
+            add(boxGenre, 1, 10);
+            add(lblAuteur, 0, 11);
+            add(boxAuteur, 1, 11);
+            add(btnSave, 1, 12);
 
             mainPane.getChildren().add(this);
 	}
--- Boogle/src/view/InsertFilmView.java
+++ Boogle/src/view/InsertFilmView.java
@@ -2,6 +2,7 @@ package view;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -10,20 +11,26 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class InsertFilmView extends GridPane{
-    private Label lblTitle, lblMovienumber, lblReg, lblDate, lblDesc, lblGenre;
-    private TextField txtTitle, txtMovienumber, txtReg, txtDate, txtGenre;
+    private Label lblTitle, lblMovienumber, lblReg, lblDate, lblDesc, lblGenre, lblActeur, lblLibary, lblNumber;
+    private TextField txtTitle, txtMovienumber, txtReg, txtDate;
+    private ComboBox boxGenre, boxActeur, boxLibary, boxNumber;
     private Text movieInput;
     private TextArea txtDesc;
     private Button btnSave;
 	
 	public InsertFilmView(Pane mainPane) {
+            lblLibary = new Label (" Bibliotheek : ");
+            lblNumber = new Label (" Kastnummer : ");
             lblTitle = new Label(" Titel : ");
             lblMovienumber = new Label(" Filmnummer : ");
             lblReg = new Label(" Regiseur : ");
             lblDate = new Label(" Premiere datum : ");
             lblDesc = new Label(" Beschrijving : ");
             lblGenre = new Label(" Genre : ");
+            lblActeur = new Label(" Acteur : ");
             
+            boxLibary = new ComboBox();
+            boxNumber = new ComboBox();
             txtTitle = new TextField();
             txtMovienumber = new TextField();
             txtReg = new TextField();
@@ -31,7 +38,8 @@ public class InsertFilmView extends GridPane{
             txtDesc  = new TextArea();
             txtDesc.setPrefHeight(150);
             txtDesc.setPrefWidth(20);
-            txtGenre = new TextField();
+            boxGenre = new ComboBox();
+            boxActeur = new ComboBox();
             
             movieInput = new Text(" Film invoeren ");
             
@@ -42,19 +50,25 @@ public class InsertFilmView extends GridPane{
             setVgap(10);
         
             add(movieInput, 0, 0); 
-            add(lblTitle, 0, 2);
-            add(txtTitle, 1, 2);
-            add(lblMovienumber, 0, 3);
-            add(txtMovienumber, 1, 3);
-            add(lblReg, 0, 4);
-            add(txtReg, 1, 4);
-            add(lblDate, 0, 5);
-            add(txtDate, 1, 5);
-            add(lblDesc, 0, 6);
-            add(txtDesc, 1, 6);
-            add(lblGenre, 0, 7);
-            add(txtGenre, 1, 7);
-            add(btnSave, 1, 8);
+            add(lblLibary, 0, 2);
+            add(boxLibary, 1, 2);
+            add(lblNumber, 0, 3);
+            add(boxNumber, 1, 3);
+            add(lblTitle, 0, 4);
+            add(txtTitle, 1, 4);
+            add(lblMovienumber, 0, 5);
+            add(txtMovienumber, 1, 5);
+            add(lblReg, 0, 6);
+            add(txtReg, 1, 6);
+            add(lblDate, 0, 7);
+            add(txtDate, 1, 7);
+            add(lblDesc, 0, 8);
+            add(txtDesc, 1, 8);
+            add(lblGenre, 0, 9);
+            add(boxGenre, 1, 9);
+            add(lblActeur, 0, 10);
+            add(boxActeur, 1, 10);
+            add(btnSave, 1, 11);
 
             mainPane.getChildren().add(this);
 		
