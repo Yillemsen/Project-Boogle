47b46ac344c2832f5b47ede981bea431c2a3f285
Yillemsen <ywillems2@student.avans.nl>
2019-05-22 12:44:51 +0200

    Boek en filmview geupdate

--- Boogle/src/view/UpdateBoekView.java
+++ Boogle/src/view/UpdateBoekView.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -16,14 +17,15 @@ import model.BoekModel;
 import model.Database;
 
 public class UpdateBoekView extends GridPane {
-	private final Label selectISBNLabel, libraryLabel, bookCaseNrLabel, genreLabel, ISBNLabel, bookNrLabel, titleLabel,
-			languageLabel, releaseDateLabel, intTitle, authorLabel, errorLabel;
-	private final TextField ISBNTextField, bookNrTextField, titleTextField, languageTextField, releaseDateTextField,
+	private final Label selectISBNLabel, libraryLabel, bookCaseNrLabel, genreLabel, ISBNLabel, titleLabel,
+			languageLabel, releaseDateLabel, intTitleLabel, descriptionLabel, authorLabel, errorLabel;
+	private final TextField ISBNTextField, titleTextField, languageTextField, releaseDateTextField,
 			intTitleTextField;
 	private final Button saveButton, addBookCaseButton, addAuthorTextFieldButton;
 	private final Text text;
 	private final ComboBox ISBNCB, libraryCB, bookCaseCB, genreCB, authorCB;
 	private final Database db;
+	private final TextArea ta;
 	private ArrayList<String> ISBNItems, libraryItems, bookCaseItems, genreItems, authorItems;
 
 	public UpdateBoekView(Pane mainPane) {
@@ -42,18 +44,17 @@ public class UpdateBoekView extends GridPane {
 		libraryLabel = new Label("Bibliotheek:");
 		bookCaseNrLabel = new Label("Boekenkast:");
 		ISBNLabel = new Label("ISBN:");
-		bookNrLabel = new Label("BoekNr:");
 		titleLabel = new Label("Titel:");
 		languageLabel = new Label("Taal:");
 		releaseDateLabel = new Label("Datum uitgave:");
-		intTitle = new Label("Internationale titel:");
+		intTitleLabel = new Label("Internationale titel:");
 		genreLabel = new Label("Genre:");
+		descriptionLabel = new Label("Beschrijving:");
 		authorLabel = new Label("Auteur");
 		errorLabel = new Label("Error, niet alle velden zijn (correct) ingevuld");
 
 		// Instantiating textfieldobjects
 		ISBNTextField = new TextField();
-		bookNrTextField = new TextField();
 		titleTextField = new TextField();
 		languageTextField = new TextField();
 		releaseDateTextField = new TextField();
@@ -63,6 +64,9 @@ public class UpdateBoekView extends GridPane {
 		saveButton = new Button("Opslaan");
 		addBookCaseButton = new Button("+");
 		addAuthorTextFieldButton = new Button("+");
+		
+		//Instantiating textareaobjects
+		ta = new TextArea();
 
 		// Instantiating textobjects
 		text = new Text("Boek aanpassen");
@@ -82,19 +86,20 @@ public class UpdateBoekView extends GridPane {
 		setPadding(new Insets(5, 5, 5, 5));
 		setHgap(5);
 		setVgap(5);
+		ta.setPrefWidth(150);
 
 		// Place textobject
 		this.add(text, 0, 0);
 
 		// Place labelobjects with for loop
-		Label[] labelObjects = { selectISBNLabel, libraryLabel, bookCaseNrLabel, genreLabel, ISBNLabel, bookNrLabel,
-				titleLabel, languageLabel, releaseDateLabel, intTitle, authorLabel };
+		Label[] labelObjects = { selectISBNLabel, libraryLabel, bookCaseNrLabel, genreLabel, ISBNLabel,
+				titleLabel, languageLabel, releaseDateLabel, intTitleLabel, descriptionLabel, authorLabel };
 		for (int i = 0; i < labelObjects.length; i++) {
 			this.add(labelObjects[i], 0, i + 1);
 		}
 
 		// Place textfieldobjects with for loop
-		TextField[] textFieldObjects = { ISBNTextField, bookNrTextField, titleTextField, languageTextField,
+		TextField[] textFieldObjects = { ISBNTextField, titleTextField, languageTextField,
 				releaseDateTextField, intTitleTextField };
 		for (int i = 0; i < textFieldObjects.length; i++) {
 			this.add(textFieldObjects[i], 1, i + 5);
@@ -108,12 +113,15 @@ public class UpdateBoekView extends GridPane {
 		this.add(authorCB, 1, 11);
 
 		// Place buttonobject
-		this.add(saveButton, 1, 13);
+		this.add(saveButton, 1, 12);
 		this.add(addBookCaseButton, 2, 3);
 		this.add(addAuthorTextFieldButton, 2, 11);
+		
+		//Place textareas
+		this.add(ta, 1, 10);
 
 		// Place errorlabelobject
-		this.add(errorLabel, 1, 14);
+		this.add(errorLabel, 1, 13);
 
 		mainPane.getChildren().add(this);
 	}
